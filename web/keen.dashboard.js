74574bf4b2575c6c613dd1a05458c16392be7413
Juan Salmerón Moya <jnssterrass@gmail.com>
2017-04-23 07:41:22 +0200

    no redirect with POST

--- web/keen.dashboard.js
+++ web/keen.dashboard.js
@@ -11,6 +11,18 @@ function drawSplineChart() {
     });
 }
 
+$('#myForm').submit(function(e){
+    e.preventDefault();
+    $.ajax({
+        url:'/Car/Edit/17/',
+        type:'post',
+        data:$('#myForm').serialize(),
+        success:function(){
+            //whatever you wanna do after the form is successfully submitted
+        }
+    });
+});
+
 $(document).ready(function () {
     drawSplineChart();
 });
