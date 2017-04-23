4a00b5d3f05304652b30e09b1310611238ff5a64
Sergio <sergio.paredes25@gmail.com>
2017-04-23 13:23:50 +0200

    change visual styles

--- web/assets/js/fharassment.js
+++ web/assets/js/fharassment.js
@@ -16,6 +16,8 @@ var response = [
 
 var chart = "";
 
+var dataA, dataB;
+
 function drawSplineChart() {
     var a = {
         data: {
@@ -37,41 +39,31 @@ function drawSplineChart() {
         }
     };
     a.bindto = '#chart1';
-    a.data.json = [
-                {date: '2016-05-16', anger: 0.9, disgust: 0.4, fear: 0.01, joy: 0.1, sadness: 0.3},
-                {date: '2016-05-17', anger: 0.8, disgust: 0.4, fear: 0.01, joy: 0.1, sadness: 0.3},
-                {date: '2016-05-18', anger: 0.34, disgust: 0.2, fear: 0.01, joy: 0.1, sadness: 0.3},
-                {date: '2016-05-19', anger: 0.24, disgust: 0.5, fear: 0.6, joy: 0.01, sadness: 0.2},
-                {date: '2016-05-20', anger: 0.21, disgust: 0.2, fear: 0.01, joy: 0.4, sadness: 0.01},
-                {date: '2016-05-21', anger: 0.2, disgust: 0.2, fear: 0.01, joy: 0.02, sadness: 0.7},
-                {date: '2016-05-22', anger: 0.1, disgust: 0.2, fear: 0.01, joy: 0.89, sadness: 0.3}
-            ];
+    a.data.json = dataA;
     var chart = c3.generate(a);
     var a2 = a;
     a2.bindto = '#chart2';
-    a2.data.json = [
-                {date: '2016-05-16', anger: 0.9, disgust: 0.4, fear: 0.01, joy: 0.1, sadness: 0.3},
-                {date: '2016-05-17', anger: 0.8, disgust: 0.4, fear: 0.01, joy: 0.1, sadness: 0.3},
-                {date: '2016-05-18', anger: 0.34, disgust: 0.2, fear: 0.01, joy: 0.1, sadness: 0.3},
-                {date: '2016-05-19', anger: 0.24, disgust: 0.5, fear: 0.6, joy: 0.01, sadness: 0.2},
-                {date: '2016-05-20', anger: 0.21, disgust: 0.2, fear: 0.01, joy: 0.4, sadness: 0.01},
-                {date: '2016-05-21', anger: 0.2, disgust: 0.2, fear: 0.01, joy: 0.02, sadness: 0.7},
-                {date: '2016-05-22', anger: 0.1, disgust: 0.2, fear: 0.01, joy: 0.89, sadness: 0.3}
-            ];
+    a2.data.json = dataB;
     var chart2 = c3.generate(a2);
 }
 
 $('#myForm').submit(function(e){
     e.preventDefault();
     $.ajax({
-        url: 'http://ab95aeee.ngrok.io:5000/recognize',
+        url: 'http://ab95aeee.ngrok.io/dummy',
         type:'post',
+        dataType: 'text',
         data:$('#myForm').serialize(),
         success:function(data){
+            var parse = JSON.parse(data);
+            dataA = parse[0].values;
+            dataB = parse[1].values;
+            drawSplineChart();
         }
     });
 });
 
+
 $(document).ready(function () {
-    drawSplineChart();
+    // drawSplineChart();
 });
