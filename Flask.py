5ed8675761b095f93d794f1ebc077f98ac1f19de
Eduard Maura <edu.maura.puig@gmail.com>
2017-04-23 05:04:56 +0200

    Add text sentimental analysis to flask api

--- Flask.py
+++ Flask.py
@@ -4,13 +4,15 @@ import re
 from flask import Flask, request, abort
 
 from SpeechToText import SpeechToText
+from ToneAnalyzer import ToneAnalyzer
 
 app = Flask(__name__)
 speech_to_text = SpeechToText()
+tone_analyzer = ToneAnalyzer()
 
 
 def parse_whats_app_chat(whats_app_file):
-    regex = r"(\d{1,2}\/\d{1,2}\/\d{2}),\s(\d{2}\:\d{2})\s-\s([^:]*)\:\s([^\n]+)"
+    regex = r"(\d{1,2}\/\d{1,2}\/\d{2}),\s(\d{1,2}\:\d{2})\s-\s([^:]*)\:\s([^\n]+)"
     matches = re.finditer(regex, whats_app_file, re.DOTALL)
 
     info = {}
@@ -22,10 +24,21 @@ def parse_whats_app_chat(whats_app_file):
             message = groups[3]
             if author not in info.keys():
                 info[author] = {"messages": [], "transcript": ""}
-            info[author]["messages"].append({"text":  message, "time": timestamp})
-            info[author]["transcript"] += message+"\n"
+            info[author]["messages"].append({"text": message, "time": timestamp})
+            info[author]["transcript"] += message + "\n"
 
     return info
+
+
+def analyze_sentiments(parsed_text):
+    tone_scores = {}
+    for author in parsed_text.keys():
+        tone_scores[author] = tone_analyzer.analyze(parsed_text[author]["transcript"])
+    return tone_scores
+
+
+def is_there_being_harassment(sentiments_by_author):
+    pass
 
 
 @app.route("/recognize", methods=["POST"])
@@ -34,7 +47,13 @@ def hello():
     if recognition_type:
         if recognition_type == "text":
             whats_app_file = request.data.decode("utf-8")
-            return json.dumps(parse_whats_app_chat(whats_app_file), indent=2)
+            parsed_text = parse_whats_app_chat(whats_app_file)
+            tone_scores = analyze_sentiments(parsed_text)
+            # if is_there_being_harassment(tone_scores):
+            #     return "Looks like " + author + " is being harassed"
+            # else:
+            #     return "I can't conclude anything"
+            return json.dumps(tone_scores, indent=2)
         elif recognition_type == "audio":
             audio_file = request.files
             speech_to_text.parse_google()
