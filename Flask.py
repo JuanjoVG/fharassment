9a207db1f8ac570980e6f91b009a9b6d414527b2
Sergio <sergio.paredes25@gmail.com>
2017-04-23 13:38:31 +0200

    bug fix

--- Flask.py
+++ Flask.py
@@ -143,20 +143,13 @@ def bye():
             {'date': '2016-05-22', 'anger': 0.1, 'disgust': 0.2, 'fear': 0.01, 'joy': 0.89, 'sadness': 0.3, 'tensor': [1, 0, 0]}
         ]},
         {'id': "juanjo", 'values': [
-            {'date': '2016-05-16', 'anger': 0.9, 'disgust': 0.4, 'fear': 0.01, 'joy': 0.1, 'sadness': 0.3,
-             'tensor': [1, 0, 0]},
-            {'date': '2016-05-17', 'anger': 0.8, 'disgust': 0.4, 'fear': 0.01, 'joy': 0.1, 'sadness': 0.3,
-             'tensor': [0, 0, 1]},
-            {'date': '2016-05-18', 'anger': 0.34, 'disgust': 0.2, 'fear': 0.01, 'joy': 0.1, 'sadness': 0.3,
-             'tensor': [1, 0, 0]},
-            {'date': '2016-05-19', 'anger': 0.24, 'disgust': 0.5, 'fear': 0.6, 'joy': 0.01, 'sadness': 0.2,
-             'tensor': [1, 0, 0]},
-            {'date': '2016-05-20', 'anger': 0.21, 'disgust': 0.2, 'fear': 0.01, 'joy': 0.4, 'sadness': 0.01,
-             'tensor': [1, 0, 0]},
-            {'date': '2016-05-21', 'anger': 0.2, 'disgust': 0.2, 'fear': 0.01, 'joy': 0.02, 'sadness': 0.7,
-             'tensor': [1, 0, 0]},
-            {'date': '2016-05-22', 'anger': 0.1, 'disgust': 0.2, 'fear': 0.01, 'joy': 0.89, 'sadness': 0.3,
-             'tensor': [1, 0, 0]}
+            {'date': '2016-05-16', 'anger': 0.01, 'disgust': 0.4, 'fear': 0.9, 'joy': 0.1, 'sadness': 0.3, 'tensor': [1, 0, 0]},
+            {'date': '2016-05-17', 'anger': 0.01, 'disgust': 0.4, 'fear': 0.8, 'joy': 0.1, 'sadness': 0.3, 'tensor': [0, 0, 1]},
+            {'date': '2016-05-18', 'anger': 0.01, 'disgust': 0.2, 'fear': 0.34, 'joy': 0.1, 'sadness': 0.3, 'tensor': [1, 0, 0]},
+            {'date': '2016-05-19', 'anger': 0.6, 'disgust': 0.5, 'fear': 0.24, 'joy': 0.01, 'sadness': 0.2, 'tensor': [1, 0, 0]},
+            {'date': '2016-05-20', 'anger': 0.01, 'disgust': 0.2, 'fear': 0.21, 'joy': 0.4, 'sadness': 0.01, 'tensor': [1, 0, 0]},
+            {'date': '2016-05-21', 'anger': 0.1, 'disgust': 0.2, 'fear': 0.2, 'joy': 0.02, 'sadness': 0.7, 'tensor': [1, 0, 0]},
+            {'date': '2016-05-22', 'anger': 0.1, 'disgust': 0.2, 'fear': 0.01, 'joy': 0.89, 'sadness': 0.3, 'tensor': [1, 0, 0]}
         ]}
     ]
     return json.dumps(startdata, indent=2)
