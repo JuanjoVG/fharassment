389f1300ea37cd1b81190518f5a4574fc38485bd
Juan Salmerón Moya <jnssterrass@gmail.com>
2017-04-23 11:54:20 +0200

    dummy backend route

--- Flask.py
+++ Flask.py
@@ -83,6 +83,38 @@ def hello():
     else:
         abort(400)
 
+@app.route("/dummy", methods=["POST"])
+def hello():
+    startdata = [
+        {'id': "sergio", 'values':[
+            {'date': '2016-05-16', 'anger': 0.9, 'disgust': 0.4, 'fear': 0.01, 'joy': 0.1, 'sadness': 0.3, 'tensor': [1, 0, 0]},
+            {'date': '2016-05-17', 'anger': 0.8, 'disgust': 0.4, 'fear': 0.01, 'joy': 0.1, 'sadness': 0.3, 'tensor': [0, 0, 1]},
+            {'date': '2016-05-18', 'anger': 0.34, 'disgust': 0.2, 'fear': 0.01, 'joy': 0.1, 'sadness': 0.3, 'tensor': [1, 0, 0]},
+            {'date': '2016-05-19', 'anger': 0.24, 'disgust': 0.5, 'fear': 0.6, 'joy': 0.01, 'sadness': 0.2, 'tensor': [1, 0, 0]},
+            {'date': '2016-05-20', 'anger': 0.21, 'disgust': 0.2, 'fear': 0.01, 'joy': 0.4, 'sadness': 0.01, 'tensor': [1, 0, 0]},
+            {'date': '2016-05-21', 'anger': 0.2, 'disgust': 0.2, 'fear': 0.01, 'joy': 0.02, 'sadness': 0.7, 'tensor': [1, 0, 0]},
+            {'date': '2016-05-22', 'anger': 0.1, 'disgust': 0.2, 'fear': 0.01, 'joy': 0.89, 'sadness': 0.3, 'tensor': [1, 0, 0]}
+        ]},
+        {'id': "juanjo", 'values': [
+            {'date': '2016-05-16', 'anger': 0.9, 'disgust': 0.4, 'fear': 0.01, 'joy': 0.1, 'sadness': 0.3,
+             'tensor': [1, 0, 0]},
+            {'date': '2016-05-17', 'anger': 0.8, 'disgust': 0.4, 'fear': 0.01, 'joy': 0.1, 'sadness': 0.3,
+             'tensor': [0, 0, 1]},
+            {'date': '2016-05-18', 'anger': 0.34, 'disgust': 0.2, 'fear': 0.01, 'joy': 0.1, 'sadness': 0.3,
+             'tensor': [1, 0, 0]},
+            {'date': '2016-05-19', 'anger': 0.24, 'disgust': 0.5, 'fear': 0.6, 'joy': 0.01, 'sadness': 0.2,
+             'tensor': [1, 0, 0]},
+            {'date': '2016-05-20', 'anger': 0.21, 'disgust': 0.2, 'fear': 0.01, 'joy': 0.4, 'sadness': 0.01,
+             'tensor': [1, 0, 0]},
+            {'date': '2016-05-21', 'anger': 0.2, 'disgust': 0.2, 'fear': 0.01, 'joy': 0.02, 'sadness': 0.7,
+             'tensor': [1, 0, 0]},
+            {'date': '2016-05-22', 'anger': 0.1, 'disgust': 0.2, 'fear': 0.01, 'joy': 0.89, 'sadness': 0.3,
+             'tensor': [1, 0, 0]}
+        ]}
+    ]
+    return json.dumps(startdata, indent=2)
+    abort(400);
+
 
 if __name__ == "__main__":
     app.run()
